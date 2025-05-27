efb0fb9a5fd65a0731b16a3470b8ef18ba6d3f47
Anurag <tiwanurag44@gmail.com>
2025-05-27 09:58:42 +0530

    Main JS file update with Redis DB

--- src/index.js
+++ src/index.js
@@ -2,18 +2,34 @@ import express from "express"
 import dotenv from "dotenv"
 dotenv.config();
 import main from "./config/db.js";
+import cookieParser from "cookie-parser";
+import authRouter from "./routes/userAuth.js";
+import redisClient from "./config/redis.js";
 
 const app=express()
 
 app.use(express.json())
+app.use(cookieParser())
 
-main()
-.then(async()=>{
+app.use("/user",authRouter)
 
-    console.log("Connected to DB")
-    app.listen(process.env.PORT,()=>{
+
+const InitializeConnection= async()=>{
+
+    try{
+          
+        await Promise.all([main(),redisClient.connect()])
+        console.log("Connected to DB")
+
+        app.listen(process.env.PORT,()=>{
     
-        console.log("Server is listening at port 3000")
-     })
-})
-.catch(err=>console.log("Error Not Connected to DB",err))
+            console.log("Server is listening at port 3000")
+         })
+    }
+    catch(err)
+    {
+        console.log("Error Not Connected to DB"+err)
+    }
+}
+
+InitializeConnection();
