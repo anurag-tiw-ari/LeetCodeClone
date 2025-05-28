f72ea95555a462b4d4abdf35d8455c7f4b624bb8
Anurag <tiwanurag44@gmail.com>
2025-05-28 09:17:14 +0530

    Problem Schema + Collection Done

---
+++ src/models/problem.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const problemSchema = new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    difficulty:{
+        type:String,
+        enum:['easy','medium','hard'],
+        required:true
+    },
+    tags:{
+        type:String,
+        enum:['Array','String','Recursion','Basic Programming','Star Pattern','Maths','LinkedList','Tree','Graph','DP','Stack','Queue'],
+        required:true
+    },
+    visibleTestCases:[
+        {
+            input:{
+                type:String,
+                required:true,
+            },
+            output:{
+                type:String,
+                required:true
+            },
+            explanation:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    hiddenVisibleTestCases:[
+        {
+            input:{
+                type:String,
+                required:true,
+            },
+            output:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    startCode:[
+        {
+            language:{
+                type:String,
+                required:true,
+            },
+            initialCode:{
+                type:String,
+                required:true
+            }
+        }
+    ],
+    problemCreator:{
+        type:Schema.Types.ObjectId,
+        ref:'user',
+        required:true
+    }
+})
+
+
+const Problem = mongoose.model('problem',problemSchema)
+
+export default Problem;
