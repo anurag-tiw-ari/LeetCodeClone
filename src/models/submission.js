27d0a363643e9f61648f6617715810e2d4c14cc8
Anurag <tiwanurag44@gmail.com>
2025-06-02 11:36:24 +0530

    Submission Schema

---
+++ src/models/submission.js
@@ -0,0 +1,58 @@
+
+import mongoose from "mongoose";
+import { Schema } from "mongoose";
+
+const submissionSchema = new Schema({
+    userId:{
+        type:Schema.Types.ObjectId,
+        ref:'User',
+        required:true
+    },
+    problemId:{
+        type:Schema.Types.ObjectId,
+        ref:'Problem',
+        required:true,
+    },
+    code:{
+        type:String,
+        required:true
+    },
+    language:{
+        type:String,
+        required:true,
+        enum:['C++','Java','JavaScript']
+    },
+    status:{
+        type:String,
+        enum:['pending','accepted','wrong answer','error'],
+        default:'pending'
+    },
+    runTime:{
+        type:Number,  //ms
+        default:0
+    },
+    memory:{
+        type:Number, //KB
+        default:0
+    },
+    errorMessage:{
+        type:String,
+        default:''
+    },
+    testCasesPassed:{
+        type:Number,
+        default:0
+    },
+    testCasesTotal:{
+        type:Number,
+        default:0
+    }
+
+},
+{
+    timestamps:true
+})
+
+const Submission = mongoose.model('submission',submissionSchema)
+
+export default Submission
