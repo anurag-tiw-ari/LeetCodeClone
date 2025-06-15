043fbb1cb4a20287d60d4882c1c8713a404e4b35
Anurag <tiwanurag44@gmail.com>
2025-06-15 13:24:33 +0530

    Content Schema Created

---
+++ src/models/content.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+import { Schema } from "mongoose";
+
+const contentSchema = new Schema( {
+    topic:{
+        type:String,
+        rqeuired:true,
+        enum:['Array','LinkedList','Binary Tree','Binary Search Tree', 'Recursion', 'Graph','Heap','Stack','Queue']
+    },
+    content:{
+        type:String,
+        reuired:true
+    },
+    title:{
+        type:String
+    },
+    ContentCreator:{
+        type:Schema.Types.ObjectId,
+        ref:'user',
+        required:true
+    }
+},
+{
+    timestamps:true
+}
+)
+
+const Content = mongoose.model('content',contentSchema)
+
+export default Content;
